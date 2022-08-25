bc449c90c5b6c1ee1454ab73371bddde9ad33d1b
tuphan-dn <tuphan@descartes.network>
2022-08-25 09:50:15 +0700

    update core

--- src/view/index.tsx
+++ src/view/index.tsx
@@ -1,5 +1,5 @@
 import { useEffect } from 'react'
-import { net, useUI } from '@sentre/senhub'
+import { net, useSetBackground } from '@sentre/senhub'
 
 import EmbededView from '@sentre/embeded-view'
 import MainnetOnly from './mainnetOnly'
@@ -11,7 +11,7 @@ const {
 } = configs
 
 const View = () => {
-  const { setBackground } = useUI()
+  const setBackground = useSetBackground()
 
   useEffect(() => {
     setBackground({ light: '#1b142b', dark: '#1b142b' })
